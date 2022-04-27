9d4485826248e88109b1c8260d916b06aeefd3ba
Laura Bigoni <laurabigoni.92@gmail.com>
2022-04-27 10:23:09 +0200

    fixed seeders

--- database/seeds/ProductSeeder.php
+++ database/seeds/ProductSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Arr;
 use Faker\Generator as Faker;
 use App\Models\Product;
 
@@ -14,8 +15,10 @@ class ProductSeeder extends Seeder
      */
     public function run(Faker $faker)
     {
+        $user_ids = Product::pluck('id')->toArray();
         for ($i = 0; $i < 15; $i++) {
             $product = new Product();
+            $product->user_id = Arr::random($user_ids);
             $product->name = $faker->word();
             $product->price = $faker->randomFloat(2, 5, 100);
             $product->description = $faker->paragraph();
--- database/seeds/UserSeeder.php
+++ database/seeds/UserSeeder.php
@@ -1,8 +1,6 @@
 <?php
 
 use App\User;
-use App\Models\Product;
-use Illuminate\Support\Arr;
 use Faker\Generator as Faker;
 use Illuminate\Database\Seeder;
 
@@ -15,10 +13,8 @@ class UserSeeder extends Seeder
      */
     public function run(Faker $faker)
     {
-        $product_ids = Product::pluck('id')->toArray();
         for ($i = 0; $i < 10; $i++) {
             $user = new User();
-            $user->product_id = Arr::random($product_ids);
             $user->restaurant_name = $faker->words(2, true);
             $user->email = $faker->email();
             $user->password = bcrypt($faker->password());
@@ -28,12 +24,5 @@ class UserSeeder extends Seeder
 
             $user->save();
         }
-        // ! Qua inseriamo piu prodotti per ogni ristorante
-        // $product_ids = Product::all('id');
-        // $user->each(function (App\User $user) use ($product_ids) {
-        //     $user->products()->attach(
-        //         $product_ids->random(rand(0, 3))->pluck('id')->toArray()
-        //     );
-        // });
     }
 }
