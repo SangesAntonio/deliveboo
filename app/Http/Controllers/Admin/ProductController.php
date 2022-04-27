bda2cb0c38897821b36bb9591d5f4205b2244490
SangesAntonio <sanges65@gmail.com>
2022-04-27 16:25:40 +0200

    added validation in productController

--- app/Http/Controllers/Admin/ProductController.php
+++ app/Http/Controllers/Admin/ProductController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers\Admin;
 use App\Models\Product;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use Illuminate\Validation\Rule;
+
 
 class ProductController extends Controller
 {
@@ -36,14 +38,36 @@ class ProductController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(Request $request, Product $product)
     {
+        $request->validate([
+            'name' => 'required|max:50| min:2',
+            'price' => 'required| max:999| min: 1| numeric',
+            'description' => 'required| min:4 | max:500',
+            'visibility' => 'boolean',
+            'image' => 'image',
+        ], [
+            'name.required' => 'Il nome del prodotto è obbligatorio',
+            'description.required' => 'La descrizione è obbligatoria',
+            'name.max' => 'La lunghezza non può essere maggiore di :max caratteri',
+            'name.min' => 'La lunghezza non può essere minore di :min caratteri',
+            'description.min' => 'La lunghezza non può essere minore di :min caratteri',
+            'description.max' => 'La lunghezza non può essere maggiore di :max caratteri',
+            'price.numeric' => 'Il prezzo deve essere un numero',
+            'price.min' => 'Il prezzo deve essere minimo :min euro',
+            'price.max' => 'Il prezzo deve essere massimo :max euro',
+            'image' => 'Il formato dell\'immagine non è corretto.',
+        ]);
         $data = $request->all();
         $product = new Product();
         $product->fill($data);
         $product->save();
         return redirect()->route('admin.products.show');
     }
+
+
+
+    // Rule::unique('product')->ignore($product->id)
 
     /**
      * Display the specified resource.
@@ -76,6 +100,24 @@ class ProductController extends Controller
      */
     public function update(Request $request, Product $product)
     {
+        $request->validate([
+            'name' => 'required|max:50| min:2',
+            'price' => 'required| max:999| min: 1| numeric',
+            'description' => 'required| min:4 | max:500',
+            'visibility' => 'boolean',
+            'image' => 'image',
+        ], [
+            'name.required' => 'Il nome del prodotto è obbligatorio',
+            'description.required' => 'La descrizione è obbligatoria',
+            'name.max' => 'La lunghezza non può essere maggiore di :max caratteri',
+            'name.min' => 'La lunghezza non può essere minore di :min caratteri',
+            'description.min' => 'La lunghezza non può essere minore di :min caratteri',
+            'description.max' => 'La lunghezza non può essere maggiore di :max caratteri',
+            'price.numeric' => 'Il prezzo deve essere un numero',
+            'price.min' => 'Il prezzo deve essere minimo :min euro',
+            'price.max' => 'Il prezzo deve essere massimo :max euro',
+            'image' => 'Il formato dell\'immagine non è corretto.',
+        ]);
         $data = $request->all();
         $product->update($data);
         return redirect()->route('admin.products.show', compact('product'));
