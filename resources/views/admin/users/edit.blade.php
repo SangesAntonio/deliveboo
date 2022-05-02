60d8d11ca846a88cf0859d7276d78709344b0ca4
SangesAntonio <sanges65@gmail.com>
2022-05-02 23:29:10 +0200

    added errors alert in form edit and create account

--- resources/views/admin/users/edit.blade.php
+++ resources/views/admin/users/edit.blade.php
@@ -5,6 +5,17 @@
       <div class="col-md-8">
         <div class="card">
           <div class="card-header">{{ __('Aggiorna il tuo ristorante') }}</div>
+
+            {{-- ERRORI FORM --}}
+          @if ($errors->any())
+            <div class="alert alert-danger">
+              <ul>
+                @foreach ($errors->all() as $error)
+                  <li>{{ $error }}</li>
+                @endforeach
+              </ul>
+            </div>
+          @endif
           <div class="card-body">
             <form action="{{ route('admin.users.update', Auth::user()->id) }}" method="POST"
               enctype="multipart/form-data">
