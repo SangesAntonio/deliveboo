530393d54b974a1801df62c7126ce04a6e6dd151
DavideVin01 <davidevinciguerradv01@gmail.com>
2022-05-02 18:44:19 +0200

    Enanches navbar (responsive)

--- resources/views/admin/users/index.blade.php
+++ resources/views/admin/users/index.blade.php
@@ -1,62 +1,61 @@
 @extends('layouts.app')
 @section('content')
+    <section id="home" class=ml-3 pt-2 d-flex align-items-center justify-content-center">
+        <div class="container-lg container-sm-fluid">
+            <div class="row">
+                <div class="col-12">
+                    <div class="card box-shadow-profile">
+                        <img class="img-fluid card-img-top w-100" src="{{ asset('/storage/' . Auth::user()->image) }}"
+                            alt="{{ Auth::user()->restaurant_name }}">
+                        <div class=" card-body d-flex flex-column align-items-center justify-content-center">
+                            {{-- nome ristorante --}}
+                            <div class="mb-5">
+                                <h2 class="card-title user-card-title">{{ Auth::user()->restaurant_name }}</h2>
 
-  <section id="home" class= ml-3 pt-2 d-flex align-items-center justify-content-center">
-    <div class="container">
-      <div class="row">
-        <div class="col-12">
-          <div class="card box-shadow-profile">
-            <img class="img-fluid card-img-top w-100" src="{{ asset('/storage/' . Auth::user()->image) }}"
-              alt="{{ Auth::user()->restaurant_name }}">
-            <div class=" card-body d-flex flex-column align-items-center justify-content-center">
-              {{-- nome ristorante --}}
-       <div class="mb-5">
-                <h2 class="card-title user-card-title">{{ Auth::user()->restaurant_name }}</h2>
-
-              </div>
+                            </div>
 
 
-              <div class="mr-auto w-100">
-                <hr>
-                {{-- email --}}
-                <div class="d-flex align-items-center">
-                  <h5><i class="fas fa-envelope h1"></i></h5>
-                  <p class="card-subtitle mb-2 mx-4">{{ Auth::user()->email }}</p>
+                            <div class="mr-auto w-100">
+                                <hr>
+                                {{-- email --}}
+                                <div class="d-flex align-items-center">
+                                    <h5><i class="fas fa-envelope h1"></i></h5>
+                                    <p class="card-subtitle mb-2 mx-4">{{ Auth::user()->email }}</p>
+                                </div>
+                                <hr>
+
+                                {{-- Indirizzo --}}
+                                <div class="d-flex align-items-center">
+                                    <h5><i class="fas fa-map-marker-alt h1"></i></h5>
+                                    <address class="card-subtitle mx-4">{{ Auth::user()->address }}</address>
+                                </div>
+                                <hr>
+
+                                {{-- Partita iva --}}
+                                <div class="d-flex align-items-center">
+                                    <i class="fas fa-building h1"></i>
+                                    <span class="mx-4">Partita IVA: {{ Auth::user()->vat_number }}</span>
+                                </div>
+                                <hr>
+
+
+                                {{-- categorie --}}
+                                <div>
+                                    <h5>Categorie:</h5>
+                                    @forelse (Auth::user()->categories as $category)
+                                        <span class="card-subtitle mb-2">{{ $category->name }}@if (!$loop->last)
+                                                ,
+                                            @endif
+                                        </span>
+                                        @empty
+                                            <span>-</span>
+                                        @endforelse
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
-                <hr>
-
-                {{-- Indirizzo --}}
-                <div class="d-flex align-items-center">
-                  <h5><i class="fas fa-map-marker-alt h1"></i></h5>
-                  <address class="card-subtitle mx-4">{{ Auth::user()->address }}</address>
-                </div>
-                <hr>
-
-                {{-- Partita iva --}}
-                <div class="d-flex align-items-center">
-                  <i class="fas fa-building h1"></i>
-                  <span class="mx-4">Partita IVA: {{ Auth::user()->vat_number }}</span>
-                </div>
-                <hr>
-
-
-                {{-- categorie --}}
-                <div>
-                  <h5>Categorie:</h5>
-                  @forelse (Auth::user()->categories as $category)
-                    <span class="card-subtitle mb-2">{{ $category->name }}@if (!$loop->last)
-                        ,
-                      @endif
-                    </span>
-                    @empty
-                      <span>-</span>
-                    @endforelse
-                  </div>
-                </div>
-              </div>
             </div>
-          </div>
-        </div>
-      </div>
-    </section>
-  @endsection
+        </section>
+    @endsection
