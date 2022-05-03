19d750e51f65d21c6415295b533db4eb45c05920
illupovero <davideluporini1996@gmail.com>
2022-05-03 15:30:11 +0200

    added ue router with npm i

--- resources/js/components/App.vue
+++ resources/js/components/App.vue
@@ -3,6 +3,7 @@
 		<div class="row justify-content-center">
 			<main class="col-12 my-4">
 				<h1>VUE HOMEPAGE</h1>
+				<!-- <router-link to="/payment">payment </router-link> -->
 				<router-view></router-view>
 			</main>
 		</div>
--- resources/js/front.js
+++ resources/js/front.js
@@ -1,9 +1,11 @@
-window.Vue = require ('vue');
+window.Vue = require('vue');
 
 import Vue from 'vue';
-import App from  './components/App.vue';
+import router from './routes';
+import App from './components/App.vue';
 
 const root = new Vue({
     el: '#root',
+    router,
     render: h => h(App)
 });
