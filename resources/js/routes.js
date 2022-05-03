2e2b12901ecfb23cb74b51f5b4e87bac6faddf31
IskahnX <davidecroce10@gmail.com>
2022-05-03 12:39:35 +0200

    added improves on the vue routes

--- resources/js/routes.js
+++ resources/js/routes.js
@@ -1,22 +1,22 @@
-// import Vue from 'vue'
-// import VueRouter from 'vue-router' 
-// Vue.use(VueRouter)
+ import Vue from 'vue'
+ import VueRouter from 'vue-router' 
+ Vue.use(VueRouter)
 
-// // import i componenti
+ // import i componenti
 
-// const router = new VueRouter ({
-//     mode: 'history',
-//     linkExactActiveClass: 'active',
-//     routes: [
-//         {path:'/', component: HomePage, name: 'home'},
-//         {path: '/products', component: ProductListPage, name: 'products'},
-//         {path: '/products/:product_name', component: SingleProductPage, name: 'product'},
-//         {path: '/restaurants', component: RestaurantListPage, name: 'restaurants'},
-//         {path: '/restaurants/:restaurant_name', component: SingleRestaurantPage, name: 'restaurant'},
-//         {path: '*', component: NotFoundPage, name: 'not-found'}
-//     ]
+ const router = new VueRouter ({
+     mode: 'history',
+     linkExactActiveClass: 'active',
+     routes: [
+         {path:'/', component: HomePage, name: 'home'},
+         {path: '/products', component: ProductListPage, name: 'products'},
+         {path: '/products/:product_name', component: SingleProductPage, name: 'product-detail'},
+         {path: '/restaurants', component: RestaurantListPage, name: 'restaurants'},
+         {path: '/restaurants/:restaurant_name', component: SingleRestaurantPage, name: 'restaurant-detail'},
+         {path: '*', component: NotFoundPage, name: 'not-found'}
+     ]
     
    
-// });
+ });
 
-// export default router
+ export default router
