b288c6fef7ab857ed54927b065c291bb359c6303
Laura Bigoni <laurabigoni.92@gmail.com>
2022-05-06 19:54:09 +0200

    moved cart

--- resources/js/components/ModalCart.vue
+++ resources/js/components/ModalCart.vue
@@ -1,28 +1,37 @@
 <template>
-  <div>
-    <b-button id="show-btn" @click="$bvModal.show('bv-modal-example')"
-      ><i class="fa-solid fa-cart-shopping position-relative"></i
-      ><span
-        v-if="cart.length"
-        class="badge badge-danger position-absolute"
-        style="top: 10%; right: 60%"
-      >
-        {{ cart.length }}
-      </span></b-button
-    >
-    <b-modal id="bv-modal-example" hide-footer>
-      <template #modal-title> </template>
-      <div class="d-block text-center">
-        <CardDetail :cart="cart" />
-      </div>
-      <b-button class="mt-3" block @click="$bvModal.hide('bv-modal-example')"
-        >Chiudi</b-button
-      >
-      <b-button class="mt-3" block @click="$bvModal.hide('bv-modal-example')"
-        >Check out</b-button
-      >
-    </b-modal>
-  </div>
+	<div>
+		<b-button
+			id="show-btn"
+			variant="primary"
+			@click="$bvModal.show('bv-modal-example')"
+			><i class="fa-solid fa-cart-shopping position-relative"></i
+			><span
+				v-if="cart.length"
+			>
+				{{ cart.length }}
+			</span></b-button
+		>
+		<b-modal id="bv-modal-example" hide-footer>
+			<template #modal-title> </template>
+			<div class="d-block text-center">
+				<CardDetail :cart="cart" />
+			</div>
+			<b-button
+				class="mt-3"
+				variant="warning"
+				block
+				@click="$bvModal.hide('bv-modal-example')"
+				>Chiudi</b-button
+			>
+			<b-button
+				variant="success"
+				class="mt-3"
+				block
+				@click="$bvModal.hide('bv-modal-example')"
+				>Check out</b-button
+			>
+		</b-modal>
+	</div>
 </template>
 
 <script>
@@ -30,14 +39,14 @@ import ProductCard from "./products/ProductCard.vue";
 import CardDetail from "./restaurants/CardDetail.vue";
 
 export default {
-  components: { ProductCard, CardDetail },
-  name: "ModelCart",
-  props: ["cart"],
-  data() {
-    return {};
-  },
-  methods: {},
-  computed: {},
+	components: { ProductCard, CardDetail },
+	name: "ModelCart",
+	props: ["cart"],
+	data() {
+		return {};
+	},
+	methods: {},
+	computed: {},
 };
 </script>
 
--- resources/js/components/restaurants/SingleRestaurantPage.vue
+++ resources/js/components/restaurants/SingleRestaurantPage.vue
@@ -1,50 +1,49 @@
 <template>
-  <div class="container">
-    <div class="row pt-5">
-      <div class="col-12 pt-md-5">
-        <h3 class="pb-3 mx-3">{{ user.restaurant_name }}</h3>
-        <b-card
-          :img-src="`/storage/${user.image}`"
-          img-alt="Card image"
-          img-left
-          class="mb-3 mx-3"
-        >
-          <b-card-text>
-            <ul>
-              <li class="text-black">{{ user.address }}</li>
-              <li class="text-black">{{ user.restaurant_name }}</li>
-              <ul v-for="category in user.categories" :key="category.id">
-                <li>
-                  {{ category.name }}
-                </li>
-              </ul>
-            </ul>
-          </b-card-text>
-        </b-card>
-      </div>
-    </div>
-
-    <div class="row">
-      <div
-        class="
-          col-12
-          d-flex
-          flex-wrap
-          align-items-center
-          justify-content-between
-        "
-      >
-        <ModalCart :cart="cart" class="fixed-top m-3" />
-        <ProductCard
-          @addProduct="addProduct"
-          @removeProduct="removeProduct"
-          v-for="(product, index) in user.products"
-          :key="index"
-          :product="product"
-        />
-      </div>
-    </div>
-  </div>
+	<div class="container">
+		<div class="row pt-5">
+			<div class="col-12 pt-md-5">
+				<h3 class="pb-3 mx-3">{{ user.restaurant_name }}</h3>
+				<b-card
+					:img-src="`/storage/${user.image}`"
+					img-alt="Card image"
+					img-left
+					class="mb-3 mx-3"
+				>
+					<b-card-text>
+						<ul>
+							<li class="text-black">{{ user.address }}</li>
+							<li class="text-black">{{ user.restaurant_name }}</li>
+							<ul v-for="category in user.categories" :key="category.id">
+								<li>
+									{{ category.name }}
+								</li>
+							</ul>
+						</ul>
+					</b-card-text>
+				</b-card>
+			</div>
+		</div>
+		<div class="row">
+			<div
+				class="
+					col-12
+					d-flex
+					flex-wrap
+					align-items-center
+					justify-content-between
+				"
+			>
+				<ProductCard
+					@addProduct="addProduct"
+					@removeProduct="removeProduct"
+					v-for="(product, index) in user.products"
+					:key="index"
+					:product="product"
+				/>
+			</div>
+		</div>
+		<ModalCart :cart="cart" class="fixed-top pt-md-5 mt-5 mr-3 cart ml-auto" />
+	</div>
 </template>
 
 
@@ -53,52 +52,55 @@ import axios from "axios";
 import ProductCard from "../products/ProductCard.vue";
 import ModalCart from "../ModalCart.vue";
 export default {
-  name: "RestaurantList",
-  components: {
-    ProductCard,
-    ModalCart,
-  },
-  data() {
-    return {
-      isLoading: false,
-      user: [],
-      cart: [],
-    };
-  },
-  methods: {
-    getRestaurant() {
-      //   this.isLoading = true;
-      axios
-        .get("http://localhost:8000/api/users/" + this.$route.params.id)
-        .then((res) => {
-          this.user = res.data;
-          console.log(res.data);
-        })
-        .catch((err) => {
-          console.error(err);
-        })
-        .then(() => {
-          // this.isLoading = false;
-        });
-    },
-    addProduct(product) {
-      this.cart.push(product);
-    },
-    removeProduct(product) {
-      const position = this.cart.indexOf(product);
-      if (position > -1) {
-        this.cart.splice(position, 1);
-      }
-    },
-  },
-  mounted() {
-    this.getRestaurant();
-  },
+	name: "RestaurantList",
+	components: {
+		ProductCard,
+		ModalCart,
+	},
+	data() {
+		return {
+			isLoading: false,
+			user: [],
+			cart: [],
+		};
+	},
+	methods: {
+		getRestaurant() {
+			//   this.isLoading = true;
+			axios
+				.get("http://localhost:8000/api/users/" + this.$route.params.id)
+				.then((res) => {
+					this.user = res.data;
+					console.log(res.data);
+				})
+				.catch((err) => {
+					console.error(err);
+				})
+				.then(() => {
+					// this.isLoading = false;
+				});
+		},
+		addProduct(product) {
+			this.cart.push(product);
+		},
+		removeProduct(product) {
+			const position = this.cart.indexOf(product);
+			if (position > -1) {
+				this.cart.splice(position, 1);
+			}
+		},
+	},
+	mounted() {
+		this.getRestaurant();
+	},
 };
 </script>
 
 <style lang="scss" scoped>
 img {
-  width: 30%;
+	width: 30%;
+}
+.cart {
+	width: 60px;
 }
 </style>
